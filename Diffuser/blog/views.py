f05d594825dd9aa796aac5961fe086b1cd2dda50
Boris Edison <borisedison@gmail.com>
2022-07-10 19:27:17 +0530

    pagination in post details and forgot password

--- Diffuser/blog/views.py
+++ Diffuser/blog/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView,
     DetailView,
     CreateView,
     UpdateView,
-    DeleteView
+    DeleteView,
 )
 from .models import Post
 
@@ -13,7 +14,7 @@ from .models import Post
 
 def home(request):
     context = {
-        'posts' : Post.objects.all()
+        'posts' : Post.objects.all().order_by('-date_posted')
         }
 
     return render(request, 'blog/home.html', context)
@@ -21,8 +22,20 @@ def home(request):
 # class PostListView(ListView):
 #     model = Post
 #     template_name = 'blog/home.html'    # <app>/<model>_<viewtype>.html
-#     context_object_name: 'posts'
+#     context_object_name = 'post'
 #     ordering = ['-date_posted']
+#     paginate_by = 2
+    
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'    # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 2
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post
