ed8534b2518aaf3ea25c7aa376e2b208489371da
BorisEdison <borisedison@gmail.com>
2022-10-25 13:25:10 +0530

    pagination and forgot password

--- Diffuser/blog/views.py
+++ Diffuser/blog/views.py
@@ -17,14 +17,14 @@ class PostListView(ListView):
     template_name = 'blog/home.html'    # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 1
+    paginate_by = 10
     
 class UserPostListView(ListView):
     model = Post
     template_name = 'blog/user_posts.html'    # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 1
+    paginate_by = 10
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
