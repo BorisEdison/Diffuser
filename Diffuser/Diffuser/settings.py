812213ceeb96f6e3e8b635f2d0341242b42bd52e
BorisEdison <borisedison@gmail.com>
2022-10-25 16:39:42 +0530

    added django-heroku

--- Diffuser/Diffuser/settings.py
+++ Diffuser/Diffuser/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,3 +145,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('DIFFUSER_EMAIL')
 EMAIL_HOST_PASSWORD = os.environ.get('DIFFUSER_PASSWORD')
+
+django_on_heroku.settings(locals())
