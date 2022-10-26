2d7b2983d5ab5aa4ca8b9aaafd76c2e576fc94e7
Ubuntu <ubuntu@ip-172-31-46-200.ap-northeast-1.compute.internal>
2022-10-26 11:00:35 +0000

    changed path of few directories

--- Diffuser/settings.py
+++ Diffuser/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ps!9+4f5ayhyz2gv993eudoxp5k79@ht%k+(_-**je@7(lzz2a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
