5e53516e6e02b5c7e84d72c02adcb1d07ec24d7c
BorisEdison <borisedison@gmail.com>
2022-11-03 20:34:06 +0530

    finalised sidebar

--- Diffuser/Diffuser/urls.py
+++ Diffuser/Diffuser/urls.py
@@ -21,7 +21,7 @@ from django.urls import path,include
 from users import views as user_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('def_admin-Page-django/', admin.site.urls),
     path('register/',user_views.register,name='register'),
     path('profile/',user_views.profile,name='profile'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
