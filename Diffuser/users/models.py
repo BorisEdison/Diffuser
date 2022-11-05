964b9de853066edd8b83715b0d3e422e2b5d1b6f
BorisEdison <borisedison@gmail.com>
2022-11-05 11:31:35 +0530

    final update

--- Diffuser/users/models.py
+++ Diffuser/users/models.py
@@ -2,19 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
+
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self, *args,**kwargs):
+    def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)   # open image of the current instance
-        
-        if img.height > 300 or img.width > 300:
-            output_size = (300,300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+        img = Image.open(self.image.path)
+        rgb_im = img.convert('RGB')
+        if rgb_im.height > 300 or rgb_im.width > 300:
+            output_size = (300, 300)
+            rgb_im.thumbnail(output_size)
+            rgb_im.save(self.image.path)
--- Diffuser/users/views.py
+++ Diffuser/users/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+
 
 def register(request):
     if request.method == 'POST':
@@ -11,18 +12,16 @@ def register(request):
             username = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
-
     else:
         form = UserRegisterForm()
-    return render(request, 'users/register.html',{'form':form})
+    return render(request, 'users/register.html', {'form': form})
 
 
 @login_required
 def profile(request):
     if request.method == 'POST':
-        u_form =  UserUpdateForm(request.POST, instance = request.user)
-        p_form =  ProfileUpdateForm(request.POST, request.FILES, instance = request.user.profile)
-
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
@@ -30,10 +29,12 @@ def profile(request):
             return redirect('profile')
 
     else:
-        u_form = UserUpdateForm(instance = request.user)
-        p_form = ProfileUpdateForm(instance = request.user.profile)
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
     context = {
         'u_form': u_form,
         'p_form': p_form
     }
+
     return render(request, 'users/profile.html', context)
